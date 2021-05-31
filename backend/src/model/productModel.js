a753013fc03bfbdaee553883cb75db6ce96beb9e
IrenePerezAlonso <irperal@hotmail.com>
2021-05-31 18:20:56 +0200

    Change the model

--- backend/src/model/productModel.js
+++ backend/src/model/productModel.js
@@ -1,40 +1,14 @@
 const { model, Schema }  = require('mongoose');
 
 const productsSchema = Schema({
-	fish: {
-		white: {
-			name: String,
-			price: Number,
-			img: String,
-			preparation: [String]
-		},
-		blue: {
-			name: String,
-			price: Number,
-			img: String,
-			preparation: [String]
-		},
-	},
-	shellfish: {
-		mollusck: {
-			name: String,
-			price: Number,
-			img: String,
-			preparation: [String]
-		},
-		crustacean: {
-			name: String,
-			price: Number,
-			img: String,
-			preparation: [String]
-		},
-	},
-	cephalopod: {
+	products:{
+		type: String,
+		subType: String,
 		name: String,
-		price: Number,
 		img: String,
-		preparation: [String]
-	},
+		price: Number,
+		isFish: Boolean
+	}
 });
 
 module.exports = model('Products', productsSchema);
