e3a1acd8543b950a2e7bab2616ec295a6f0c377e
IrenePerezAlonso <irperal@hotmail.com>
2021-06-01 09:50:53 +0200

    Fix the user model

--- backend/src/model/usersModel.js
+++ backend/src/model/usersModel.js
@@ -3,10 +3,10 @@ const { model, Schema }  = require('mongoose');
 const userSchema = Schema({
 		email: String,
 		password: String,
-		cart: {
+		cart: [{
 			id: String,
 			quantity: Number,
-		}
+		}]
 });
 
 module.exports = model('products', userSchema);
