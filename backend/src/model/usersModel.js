26736b36dca28199404f5551c2e8197b6bd0ce90
IrenePerezAlonso <irperal@hotmail.com>
2021-06-01 10:19:51 +0200

    Add more propertys into the user model

--- backend/src/model/usersModel.js
+++ backend/src/model/usersModel.js
@@ -1,12 +1,15 @@
 const { model, Schema }  = require('mongoose');
 
 const userSchema = Schema({
-		email: String,
-		password: String,
-		cart: [{
-			id: String,
-			quantity: Number,
-		}]
+	email: String,
+	password: String,
+	name: String,
+	addres: String,
+	phone: Number,
+	cart: [{
+		id: String,
+		quantity: Number,
+	}]
 });
 
 module.exports = model('products', userSchema);
