b8c3155d1af8b61aee9423f1e80bb10431f7521b
IrenePerezAlonso <irperal@hotmail.com>
2021-06-01 09:36:53 +0200

    Create an userModel

---
+++ backend/src/model/usersModel.js
@@ -0,0 +1,12 @@
+const { model, Schema }  = require('mongoose');
+
+const userSchema = Schema({
+		email: String,
+		password: String,
+		cart: {
+			id: String,
+			quantity: Number,
+		}
+});
+
+module.exports = model('products', userSchema);
