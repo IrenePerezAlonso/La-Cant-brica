5ae4f129d66ac2c0ce9cbcdc24c9344aa63ec20a
IrenePerezAlonso <irperal@hotmail.com>
2021-06-01 12:56:19 +0200

    Create an auth.routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,21 +1,102 @@
+const passport = require('passport');
 const { Router } = require('express');
-const productsController = require('../controllers/productsController')();
+const jwt = require('jsonwebtoken');
 
-function productsRouter() {
-    const routes = Router();
+let refreshTokens = [];
+const authRoutes = Router();
 
-    routes
-        .route('/')
-        .get(productsController.getAll)
-        .post(productsController.createOne);
-    
-    routes
-        .route('/:productId')
-        .get(productsController.getById)
-        .put(productsController.updateById)
-        .delete(productsController.deleteById);
+authRoutes.post(
+  '/signup',
+  passport.authenticate('signup', { session: false }),
+  async (req, res) => {
+    res.json({
+      message: 'Signup successful',
+      user: req.user,
+    });
+  },
+);
 
-    return routes;
-}
+authRoutes.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
 
-module.exports = productsRouter();
+            return next(error);
+          }
+
+          return req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+
+              const data = { _id: user._id, email: user.email };
+              const token = jwt.sign(
+                { user: data },
+                process.env.JWT_SECRET,
+                { expiresIn: '60m' },
+              );
+              const refreshToken = jwt.sign(
+                { user: data },
+                process.env.JWT_SECRET,
+              );
+
+              refreshTokens.push(refreshToken);
+
+              return res.json({
+                token,
+                refreshToken,
+              });
+            },
+          );
+        } catch (error) {
+          return next(error);
+        }
+      },
+    )(req, res, next);
+  },
+);
+
+authRoutes.post('/token', (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  if (!refreshTokens.includes(token)) {
+    return res.sendStatus(403);
+  }
+
+  return jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    const data = { _id: user._id, email: user.email };
+
+    const accessToken = jwt.sign(
+      { user: data },
+      process.env.JWT_SECRET,
+      { expiresIn: '60m' },
+    );
+
+    return res.json({
+      accessToken,
+    });
+  });
+});
+
+authRoutes.post('/logout', (req, res) => {
+  const { token } = req.body;
+  refreshTokens = refreshTokens.filter((current) => current !== token);
+
+  res.send('Logout successful');
+});
+
+module.exports = authRoutes;
