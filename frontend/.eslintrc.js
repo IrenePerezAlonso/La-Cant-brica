b41acc82320c6eba0637e2dbe7348c163c45bf96
IrenePerezAlonso <irperal@hotmail.com>
2021-06-02 11:31:18 +0200

    Change eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,27 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+      browser: true,
+      es2021: true
     },
-    "extends": "plugin:react/recommended",
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'airbnb-typescript'
     ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 12,
+      sourceType: 'module'
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint'
+    ],
+    ignorePatterns: ['.eslintrc.js'],
     rules: {
-        'comma-dangle': [2, 'never'],
-        'linebreak-style': 0,
-        'global-require': 0,
-        'eslint linebreak-style': [0, 'error', 'windows'],
-        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }]
-      }
+      "eol-last": 0,
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+      'comma-dangle': ['error', 'never'],
+      'linebreak-style': 0,
+      'global-require': 0,
+      'eslint linebreak-style': [0, 'error', 'windows'],
+      'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+      'react/jsx-one-expression-per-line': 'off',
+      'comma-dangle': 'off',
+    }
 };
