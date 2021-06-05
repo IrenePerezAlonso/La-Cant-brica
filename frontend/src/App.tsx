482359e1beb3f34e5c529302078b60425356444e
Irene Perez Alonso <irperal@hotmail.com>
2021-06-05 13:24:41 +0200

    Add the header and footer into the app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
-// import Header from './components/0-Header/index';
+import Header from './components/0-Header/index';
 // import Dashboard from './components/1-Dashboard/index';
-// import Footer from './components/0-Footer/index';
+import Footer from './components/0-Footer/index';
 import List from './components/2-List/index';
 
 function App() {
   return (
     <Provider store={store({ type: String })}>
+      <Header />
       <List />
-      {/* <Header />
-      <Dashboard />
-      <Footer /> */}
+      {/* <Dashboard /> */}
+      <Footer />
     </Provider>
   );
 }
