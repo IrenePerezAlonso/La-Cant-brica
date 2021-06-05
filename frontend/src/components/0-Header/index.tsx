e939fb2a1840de86eac57f4369d9bc2a8b7f2245
Irene Perez Alonso <irperal@hotmail.com>
2021-06-05 13:24:14 +0200

    Add the styles into the header file

--- frontend/src/components/0-Header/index.tsx
+++ frontend/src/components/0-Header/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
+import './header.css';
 import {
   BrowserRouter,
   Route,
   Switch,
   Link
 } from 'react-router-dom';
-import logo from '../../assets/logo.png';
 import Login from './Login/index';
+import loginImg from '../../assets/user_login.png';
+import logoImg from '../../assets/logo.png';
+import cartImg from '../../assets/shopping_cart.png';
 
 function Header() {
   return (
     <BrowserRouter>
       <header className="header">
         <nav className="header-login">
-          <Link to="/">Inicio</Link>
-          <Link to="/Login">Login</Link>
+          <Link to="/" className="header-login__start">Inicio</Link>
+          <Link to="/Login" className="header-login__login">
+            <img src={loginImg} alt="login" />
+          </Link>
         </nav>
         <div className="header-nav">
-          <img src={logo} alt="" />
+          <img src={logoImg} alt="logo de La Cantábrica" className="header-nav__logo" />
+          <img src={cartImg} alt="cart" className="header-nav__cart" />
         </div>
       </header>
       <Switch>
