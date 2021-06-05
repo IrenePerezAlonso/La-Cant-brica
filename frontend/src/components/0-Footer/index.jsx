2c80840a376e50c9df6ca0a001c9f51c8b827b2d
Irene Perez Alonso <irperal@hotmail.com>
2021-06-05 13:24:25 +0200

    Add the styles into the footer file

--- frontend/src/components/0-Footer/index.jsx
+++ frontend/src/components/0-Footer/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from '../../assets/mercadocentral-logo.png';
+import logoImg from '../../assets/mercadocentral-logo.png';
+import './footer.css';
 
 function Footer() {
   return (
-    <footer className="App">
-      <img src={logo} alt="" />
-      <div>
-        <p>guía de compra</p>
-        <p>
+    <footer className="footer">
+      <img src={logoImg} alt="logo mercado" className="footer-logo" />
+      <div className="footer-text">
+        <p className="footer-text__title">
+          🐟 guía de compra
+        </p>
+        <p className="footer-text__phrase">
           Envío gratuito para Salamanca y alfoz.
           Pago en efectivo o tarjeta.
         </p>
