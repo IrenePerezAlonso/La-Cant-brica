3a05ced8ef6989a9c66d7894714225dba2455c11
Irene Perez Alonso <irperal@hotmail.com>
2021-06-07 11:36:18 +0200

    Create the dashboard style

--- frontend/src/components/1-Dashboard/index.tsx
+++ frontend/src/components/1-Dashboard/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import './dashboard.css';
 
 function Dashboard() {
   return (
     <>
-      <div className="div">
-        <div>
-          <h2>LA CANTÁBRICA</h2>
-          <p>
+      <div className="dashboard">
+        <div className="dashboard-content">
+          <h2 className="dashboard-content__title">LA CANTÁBRICA</h2>
+          <p className="dashboard-content__phrase">
             Ofrecemos los mejores, más frescos y seleccionados pescados
             y mariscos, frescos y congelados. Fileteamos todos los productos que vendemos.
           </p>
+          <button type="button" className="dashboard-content__button button">ir a la tienda</button>
         </div>
       </div>
     </>
