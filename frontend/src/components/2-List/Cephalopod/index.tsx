9da7a75b78d5d368d120e78bd047bff6e2817e38
Irene Perez Alonso <irperal@hotmail.com>
2021-06-07 09:33:08 +0200

    Create the cephalopod component

---
+++ frontend/src/components/2-List/Cephalopod/index.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, FC } from 'react';
+import { connect } from 'react-redux';
+import loadProducts from '../../../redux/actions/actionCreator';
+import Products from '../../../types/products';
+import '../list.css';
+
+type CephalopodProps = {
+    products: Products [],
+    dispatch: any
+}
+
+const CephalopodList: FC <CephalopodProps> = ({ products, dispatch }) => {
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, []);
+
+  const CEPHALOPOD = 'cephalopod';
+
+  const cephalopodList = products.filter((cephalopod) => cephalopod.type === CEPHALOPOD);
+
+  return (
+    <>
+      <h2 className="productList-products__title-fish">cefalópodos</h2>
+      <hr className="hr1" />
+      <ul className="productList-products__ul">
+        {
+          cephalopodList.map((cephalopod) => (
+            <li className="ul-list">
+              <div>
+                <img src={cephalopod.img} alt="fish" className="ul-list__img" />
+                {cephalopod.name}
+                {cephalopod.price}
+                {' '}
+                €
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
+
+function mapStateToProps(store: any) {
+  return { products: store.productsStore };
+}
+
+export default connect(mapStateToProps)(CephalopodList);
