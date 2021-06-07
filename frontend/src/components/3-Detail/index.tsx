d250bf90ffa86264b2603be7420709ae75423f04
Irene Perez Alonso <irperal@hotmail.com>
2021-06-07 17:41:54 +0200

    Create the Detail component

---
+++ frontend/src/components/3-Detail/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import React, { FC } from 'react';
+import { connect } from 'react-redux';
+import Products from '../../types/products';
+// import ProductDetail from './SelectProduct/index';
+
+type DetailProps = {
+    products: Products [],
+    dispatch: any
+}
+
+const Detail: FC<DetailProps> = () => (
+  <div className="productList">
+    <h1 className="productList-title">TIENDA</h1>
+    <div className="productList-products">
+      {/* <ProductDetail /> */}
+    </div>
+  </div>
+);
+
+function mapStateToProps(store: any) {
+  return { products: store.productsStore };
+}
+
+export default connect(mapStateToProps)(Detail);
