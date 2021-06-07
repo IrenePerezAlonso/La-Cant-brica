d1f1e047a665d7b795159ac720c54684af973f8e
Irene Perez Alonso <irperal@hotmail.com>
2021-06-07 17:42:53 +0200

    Only import the dashboard into the app.jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
 import Header from './components/0-Header/index';
-// import Dashboard from './components/1-Dashboard/index';
+import Dashboard from './components/1-Dashboard/index';
+// import List from './components/2-List/index';
 import Footer from './components/0-Footer/index';
-import List from './components/2-List/index';
 
 function App() {
   return (
     <Provider store={store({ type: String })}>
       <Header />
-      <List />
-      {/* <Dashboard /> */}
+      <Dashboard />
       <Footer />
     </Provider>
   );
