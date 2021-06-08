1bf971b20dcf08bd5740a40f9aa5bea9794475a7
Irene Perez Alonso <irperal@hotmail.com>
2021-06-08 12:29:51 +0200

    Create an actionLogin

---
+++ frontend/src/redux/actions/actionLogin.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import actionTypes from './actionTypes';
+
+const url = 'http://localhost:2021/login';
+
+export default function getUser(email: string, password: string) {
+  return async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.post(url, { email, password });
+      dispatch({
+        type: actionTypes.POST_LOGIN,
+        user: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.POST_ERROR_LOGIN
+      });
+    }
+  };
+}
