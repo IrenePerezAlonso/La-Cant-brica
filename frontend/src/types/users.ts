3a770d6eb672b7715367f28e742fa3c74ae3020d
Irene Perez Alonso <irperal@hotmail.com>
2021-06-08 15:29:44 +0200

    Change refreshToken fot token into the user interface

--- frontend/src/types/users.ts
+++ frontend/src/types/users.ts
@@ -1,9 +1,9 @@
 export default interface Users {
     email: string,
     password: string,
-    refreshToken: string,
+    token: string,
     name: string,
     addres: string,
     phone: number,
     cart: Array<object>
-};
+}
