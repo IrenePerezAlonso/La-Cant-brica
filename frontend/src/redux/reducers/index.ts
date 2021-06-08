a38d9ad761e5638429aee9f765515bab3405cde3
Irene Perez Alonso <irperal@hotmail.com>
2021-06-08 17:55:00 +0200

    Add the cart into the reducer's index

--- frontend/src/redux/reducers/index.ts
+++ frontend/src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import productsReducer from './productsReducer';
 import selectedProductReducer from './selectedProductReducer';
+import cartReducer from './cartReducer';
 
 const rootReducer = combineReducers({
   productsStore: productsReducer,
-  selectedProduct: selectedProductReducer
+  selectedProduct: selectedProductReducer,
+  cart: cartReducer
 });
 
 export default rootReducer;
