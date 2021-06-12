3e1edeefe91f699007df742e5071a387fa2fc580
Irene Perez Alonso <irperal@hotmail.com>
2021-06-12 14:11:38 +0200

    Create the test-utils file

---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+// test-utils.js
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './redux/store/index';
+
+function render(
+  ui: any,
+  { initialState, ...renderOptions }: any = {}
+) {
+  function Wrapper({ children }: any) {
+    return (
+      <Provider store={configureStore(initialState)}>
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render };
