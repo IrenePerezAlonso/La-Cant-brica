7b7ca3f11043bf917b0e82472a97b1d4f75c3a08
Irene Perez Alonso <irperal@hotmail.com>
2021-06-15 11:42:04 +0200

    Create a actionCreator test

---
+++ frontend/src/redux/actions/actionCreator.test.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+import {
+  loadProducts,
+  getProductById
+} from './actionCreator';
+
+jest.mock('axios');
+
+describe('Given an actionCreators', () => {
+  test('loadProducts should return an action, with an actionType LOAD_PRODUCTS', async () => {
+    const dispatch = jest.fn();
+    const data = 'Bacalao';
+    axios.mockResolvedValue(data);
+
+    await loadProducts()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: actionTypes.LOAD_PRODUCTS,
+      products: data
+    });
+  });
+});
